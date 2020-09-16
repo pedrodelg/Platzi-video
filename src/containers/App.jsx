fcf162ac3c4b04a2e5c60c9e44287b75ee98d942
pedrodelg <pedro7jd@gmail.com>
2020-09-15 23:34:52 -0500

    conectando la informacion a la API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from  '../components/Categories';
@@ -8,25 +8,54 @@ import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
+const App = () => {
 
-const App = () => (
-    <div className = "App">
-        <Header/>
-        <Search/>
+    const [ videos, setVideos ] = useState({mylist: [], trends: [], originals: [] });
 
-        <Categories>
-            <Carousel>
-                <CarouselItem />
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+        .then(response => response.json())
+        .then(data => setVideos(data))
+    }, [])
+    
+    return (
+        <div className = "App">
+            <Header/>
+            <Search/>
 
-                <CarouselItem />
+        {videos.mylist.length > 0 &&
+           (<Categories title="Mi Lista">
+                <Carousel>                  
+                    <CarouselItem />
+                </Carousel>
+            </Categories>)  
+        }
+           
 
-                <CarouselItem />
-            </Carousel>
-        </Categories>    
+            <Categories title= "Tendencias">
+                <Carousel>
 
-        <Footer />
+                    {videos.trends.map(item =>
+                        <CarouselItem  key= {item.id } {... item}/>
+                    )}
+                    
 
-    </div>
-);
+                </Carousel>
+            </Categories> 
+
+            <Categories title= "Originales de Platzi Video">
+                <Carousel>
+                    <CarouselItem />
+
+                    <CarouselItem />
+
+                </Carousel>
+            </Categories> 
+
+            <Footer />
+
+        </div>
+    );
+}
 
 export default App;
